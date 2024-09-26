2107b58786b69d955055453100d3c6590978fcca
Sadman Shoumik <sadman.shoumik@welldev.io>
2024-09-26 18:21:30 +0600

    Injected Service UnitOfWork

--- ProductManagement/api/Program.cs
+++ ProductManagement/api/Program.cs
@@ -2,6 +2,8 @@
 
 
 using data_access.Context;
+using data_access.Implementation;
+using domain.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,6 +17,10 @@ builder.Services.AddSwaggerGen();
 
 // Add Entity Framework
 builder.Services.AddDbContext<ProductManagementDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ProductConnection")));
+
+
+// Add Unit Of Work Dependency
+builder.Services.AddTransient<IUnitOfWork, UnitOfWork>(); // Whenever it encounters IUnitOfWork, It goes to find UnitOfWork
 
 
 var app = builder.Build();
